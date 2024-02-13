134a5daa5ff7a23fb82251ddca1184c2144a38bf
mronzon <mathis.ronzon@gmail.com>
2024-02-13 23:06:35 +0100

    Add Chakra to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" "aside"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" backgroundColor="gold">
+        Navbar
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" backgroundColor="orange">
+          Aside
+        </GridItem>
+      </Show>
+      <Show below="lg">
+        <GridItem area="aside" backgroundColor="blue">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" backgroundColor="red">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
